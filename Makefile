b67f26cf24d7995370b10b5c1703d9e7839f0dcb
Robbie Tulip <robbie@brnkl.io>
2023-09-11 20:53:46 -0400

    Read file from command line arguments

--- Makefile
+++ Makefile
@@ -4,12 +4,13 @@ CC_OPTIONS = -I. -Wall -Wextra
 usage:
 	@echo "Usage:"
 	@echo "    * Build Compiler: 'make build'"
+	@echo "    * Run Compiler:   'make run file=<file>'"
 
 build: setup
 	@gcc -o $(BUILD_DIR)/main main.c $(CC_OPTIONS)
 
-run:
-	./$(EXE) $(file) 
+run: build
+	./$(BUILD_DIR)/main $(file) 
 
 clean:
 	@rm -rf $(BUILD_DIR)
--- main.c
+++ main.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
+void usage(void)
+{
+    printf("Usage: `make run file=<file>`\n");
+}
 
-    printf("Hello Haystack\n");
+int main(int argc, const char** argv) {
+
+    int exit_code = 0;
+
+    do {
+        if (argc != 2)
+        {
+            printf("ERROR: Expected a file to compile\n");
+            usage();
+            exit_code = -1;
+            break;
+        }
+
+
+        const char* file_path = argv[1];
+        printf("File: %s\n", file_path);
+        FILE* file = fopen(file_path, "r");
+
+        if (file == NULL)
+        {
+            printf("ERROR: Failed to open file: `%s`\n", argv[1]);
+            usage();
+            exit_code = -1;
+            break;
+        }
+
+        // File is open and must be closed
+        do {
+
+            fseek (file, 0, SEEK_END);
+            size_t length = ftell (file);
+            fseek (file, 0, SEEK_SET);
+
+            char* buffer = malloc(length);
+            if (buffer == NULL)
+            {
+                printf("ERROR: Failed to allocate space to read file.\n");
+                exit_code = -1;
+                break;
+            }
+
+            printf("Length: %ld\n", length);
+            
+            do {
+                fread(buffer, 1, length, file);
+                
+                printf("%s\n", buffer);
+
+            } while (0); // free the buffer
+
+            free(buffer);
+
+        } while(0); // Close file
+
+        fclose(file);
+        
+    } while (0);
+
+    return exit_code;
 }
